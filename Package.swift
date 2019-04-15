d7133c7b33fc81a32b29d8847b127550414b5e69
Ingmar Stein <IngmarStein@users.noreply.github.com>
2019-04-15 23:07:40 +0200

    Use swift-pm standard directory layout

--- Package.swift
+++ Package.swift
@@ -20,11 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CommandLineKit",
-            dependencies: [],
-            path: "CommandLineKit"),
+            dependencies: []),
         .testTarget(
             name: "CommandLineKitTests",
-            dependencies: ["CommandLineKit"],
-            path: "Tests"),
+            dependencies: ["CommandLineKit"]),
     ]
 )
